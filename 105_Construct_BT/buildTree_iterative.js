8b9025a17559347d73284848e1a7d9b15bf56587
Yanqing Liu <touch3247@gmail.com>
2021-10-10 16:47:59 -0700

    105 Construct Binary Tree interatively

--- 105_Construct_BT/buildTree_iterative.js
+++ 105_Construct_BT/buildTree_iterative.js
@@ -9,6 +9,9 @@ const { TreeNode, treeToArray }  = require('../Data_Structure/BTreeNode');
  * }
  */
 
+/**
+ * Put 
+ */
 var buildTree = function(preorder, inorder) {
     // sanity check
     if(preorder.length <= 0 || inorder.length <= 0){
@@ -21,44 +24,90 @@ var buildTree = function(preorder, inorder) {
         acc[cur] = i;
         return acc;
     }, index_map);
-    console.log(index_map);
 
+
+    const stack = [];
+
+    // put root node into stack
     let p = 0; 
     let root = preorder[p];
-    let node = new TreeNode(preorder[p++]);;
-    const stack = [node, [0, index_map[root]], [index_map[root] + 1, preorder.length]];
-    let top;
+    let root_index = index_map[root];
+    let tree = new TreeNode(preorder[p++]);;
+    stack.push({
+        node: tree, 
+        ls: 0 < root_index ? 0: Number.MAX_VALUE,
+        le: 0 < root_index ? root_index: Number.MIN_VALUE,
+        rs: root_index + 1 < preorder.length ? root_index+1 : Number.MAX_VALUE,
+        re: root_index + 1 < preorder.length ? preorder.length : Number.MIN_VALUE,
+    });
+
+
+    let top; 
+    let lflag = true;
+    let rflag = true;
 
     while(p < preorder.length && stack.length > 0) {
-        root = preorder[p];
-        top = stack[stack.length -1];
+        root = preorder[p++]; 
+        root_index = index_map[root]; 
+        top = stack[stack.length -1]; 
 
-        let s = top[1][0];
-        let e = top[1][1];
-        let root_index = index_map[root];
-        if(s <= root && root < e){ // root is in left 
+        const {node, ls, le, rs, re} = top;
+
+        if(ls === Number.MAX_VALUE && rs === Number.MAX_VALUE){ // already processed both left subtree and right subtree.
+            stack.pop();
+            p--
+        } else if(ls <= root_index && root_index < le) { // current node is in the left subtree of top 
             node.left = new TreeNode(root);    
-            node = node.left;
-            stack.push([node, s < root_index? [s, root_index] : null, root_index + 1 < e? [ root_index+1, e]: null);
-            top[1] = null;
-        } else {
-            s = top[2][0];
-            e = top[2][1];
-            root_index = index_map[root];
-            if(s <= root && root < e){ // root is in right 
-                node.right= new TreeNode(root);    
-                node = node.right;
-                stack.push([node, s < root_index? [s, root_index] : null, root_index + 1 < e? [ root_index+1, e]: null);
-                top[2] = null;
+            lflag = ls < root_index;
+            rflag = root_index + 1 < le;
+
+            // if both lflag and rflag are false, means the current node is a leaf node, no need to push into stack
+            if (lflag || rflag) { 
+                stack.push({
+                    node: node.left,
+                    ls: lflag ? ls : Number.MAX_VALUE,
+                    le: lflag ? root_index : Number.MIN_VALUE,
+                    rs: rflag ? root_index+1 : Number.MAX_VALUE,
+                    re: rflag ? le : Number.MIN_VALUE,
+                });
             }
+
+            // processing left subtree of top node, invalidate the it so it can be poped when comes back
+            top.ls = Number.MAX_VALUE; 
+            top.le = Number.MIN_VALUE;
+
+        } else if(rs <= root_index && root_index < re) {
+            node.right= new TreeNode(root);    
+
+            lflag = rs < root_index;
+            rflag = root_index+1 < re;
+
+            // if both lflag and rflag are false, means the current node is a leaf node, no need to push into stack
+            if (lflag || rflag) { 
+                stack.push({
+                    node: node.right,
+                    ls: lflag ? rs : Number.MAX_VALUE,
+                    le: lflag ? root_index : Number.MIN_VALUE,
+                    rs: rflag ? root_index+1 : Number.MAX_VALUE,
+                    re: rflag ? re : Number.MIN_VALUE,
+                });
+            }
+
+            // processing right subtree of top node, invalidate the it so it can be poped when comes back
+            top.rs = Number.MAX_VALUE; 
+            top.re = Number.MIN_VALUE;
+        } else {
+            console.log('something is wrong');
         }
-         
     }
+    return tree;
 }
 
 /**
  * Test case
  */
 
-console.log(treeToArray(buildTree([-1], [-1])));
+// console.log(treeToArray(buildTree([-1], [-1])));
+//console.log(treeToArray(buildTree([2, 1, 9], [1, 2, 9])));
 console.log(treeToArray(buildTree([3,9,20,15,7], [9,3,15,20,7])));
+// console.log(treeToArray(buildTree([12,5,2,9,18,15,13,14,19], [2,5,9,12,13,14,15,18,19])));
